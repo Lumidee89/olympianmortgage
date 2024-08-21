561a6454255af5896b16002bb9936dbb31debb16
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2024-08-21 17:22:07 +0100

    Ok

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 const allowedOrigins = [
     "http://localhost:5173",
+    "https://olympian-mortgage.vercel.app"
     // "https://crawler-integration.netlify.app",
     // "https://crawlertest.netlify.app",
   ];
