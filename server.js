a42f0fb34f658ed69593bb56c7e5c96269e656fb
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2025-01-26 12:00:55 +0100

    vv

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use("/api/notifications", notificationRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/contacts", contactRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
